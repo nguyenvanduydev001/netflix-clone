0810ea358fbb01238284f11131ba568834cfd09a
nguyenvanduydev001 <nguyenvanduydev001@gmail.com>
2024-05-20 13:54:44 +0700

    Create Login and Registration feature using Firebase

---
+++ src/firebase.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "firebase/app";
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    signInWithEmailAndPassword,
+    signOut,
+} from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDkqKK5sbaQrdTKIKtz2qPuirROzRIBldo",
+    authDomain: "netflix-clone-670d3.firebaseapp.com",
+    projectId: "netflix-clone-670d3",
+    storageBucket: "netflix-clone-670d3.appspot.com",
+    messagingSenderId: "308240026309",
+    appId: "1:308240026309:web:b51f8ad2f5c765e1f77eac",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+};
+
+const login = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+};
+
+const logout = () => {
+    signOut(auth)
+}
+
+export { auth, db, login, signup, logout };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
+import { login, signup } from '../../firebase';
 
 const Login = () => {
     const [signState, setSignState] = useState("Sign In");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const user_auth = async (event) => {
+        event.preventDefault();
+        if (signState === "Sign In") {
+            await login(email, password);
+        } else {
+            await signup(name, email, password);
+        }
+    };
 
     return (
         <div className="login">
@@ -11,32 +24,28 @@ const Login = () => {
             <div className="login-form">
                 <h1>{signState}</h1>
                 <form>
-                    {signState === "Sign Up" ? (
-                        <input type="text" placeholder="Your name" />
-                    ) : (
-                        <></>
-                    )}
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <button>{signState}</button>
+                    {signState === "Sign Up" ? <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Your name" /> : <></>}
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" />
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+                    <button onClick={user_auth} type="submit">{signState}</button>
                     <div className="form-help">
                         <div className="remember">
                             <input type="checkbox" />
-                            <label htmlFor="">Remember Me</label>
+                            <label>Remember Me</label>
                         </div>
                         <p>Need Help?</p>
                     </div>
                 </form>
                 <div className="form-switch">
-                    {signState === "Sign In" ? (
+                    {signState === "Sign In" ?
                         <p>
-                            New to Netflix? <span onClick={() => { setSignState("Sign Up") }}>Sign Up Now</span>
+                            New to Netflix? <span onClick={() => setSignState("Sign Up")}>Sign Up Now</span>
                         </p>
-                    ) : (
+                        :
                         <p>
-                            Already have account? <span onClick={() => { setSignState("Sign In") }}>Sign In Now</span>
+                            Already have an account? <span onClick={() => setSignState("Sign In")}>Sign In Now</span>
                         </p>
-                    )}
+                    }
                 </div>
             </div>
         </div>
